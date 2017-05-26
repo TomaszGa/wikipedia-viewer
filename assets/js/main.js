8ac08defa6cc84f66d2e391b598c350de8240ec1
Tomasz G <tomasz.galina@gmail.com>
2017-05-26 10:13:23 +0200

    Upload working version

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+
+    //focus on text field
+    $("#searchField").focus();
+
+    //search button is clicked
+    $("#searchSubmit").click(function () {
+        wikiView.searchQuery();
+    });
+
+    //enter is pressed with text field focused
+    $(document).on("keypress", "#searchField", function (e) {
+        if (e.which === 13) {
+            wikiView.searchQuery();
+        }
+    });
+
+});
+
+const wikiView = {
+    searchQuery: function() {
+        $(".message").html("");
+        const str = $("#searchField").val();
+        if (str !== "") {
+            const linkString = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + str;
+            $.getJSON(linkString, function (json) {
+                $.each(json.query.pages, function () {
+                    let htmlString = "";
+                    htmlString += "<a href='https://en.wikipedia.org/?curid=" + this.pageid + "'><div class='result-box'><h5><b>" + this.title + ":</b></h5><p> " + this.extract + "</p></div></a>";
+                    $(".message").append(htmlString);
+                    $("#footer").fadeOut(1000);
+
+                });
+
+            });
+        //in case of empty text field
+        } else {
+            $(".message").append("<h2>Nothing entered</h2>");   
+        }
+    }
+}
